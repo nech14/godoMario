5a0c4c3f3a78cc15e8d500ab3ab9fd30048d16af
nech14 <nech14@mail.ru>
2024-12-07 08:12:08 +0800

    create base enemy

---
+++ objects/enemy/base_enemy.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+var damage = 1
+@export var maxHealth = 1
+@onready var currentHealth: int = maxHealth
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
